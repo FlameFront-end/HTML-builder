ea7af478b2ba8e2e0326205a4a2c333cffdf85a7
Flame <5017_30@mail.ru>
2023-05-08 22:57:49 +0300

    feat: add solution fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir() {
+  const sourceDir = path.join(__dirname, 'files');
+  const destDir = path.join(__dirname, 'files-copy');
+
+  try {
+    await fs.access(destDir);
+  } catch (error) {
+    await fs.mkdir(destDir, { recursive: true });
+  }
+
+  const files = await fs.readdir(sourceDir);
+
+  for (const file of files) {
+    const sourceFile = path.join(sourceDir, file);
+    const destFile = path.join(destDir, file);
+    const fileStat = await fs.stat(sourceFile);
+    if (fileStat.isDirectory()) {
+      await copyDir(sourceFile, destFile);
+    } else {
+      await fs.copyFile(sourceFile, destFile);
+    }
+  }
+}
+
+copyDir()
+    .then(() => console.log('Copy completed'))
+    .catch((err) => console.error(err));
